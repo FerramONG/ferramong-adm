d57cd918acce37a289b0e8b6eb93290d5cf486d1
Arruda2904 <62766364+Arruda2904@users.noreply.github.com>
2021-04-18 18:01:31 -0300

    Agora tá tudo instalado certinho pra funcionar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Routes from "./routes/routes";
 
 function App() {
   return (
     <div>
-     teste
+     <Routes></Routes>
     </div>
   );
 }

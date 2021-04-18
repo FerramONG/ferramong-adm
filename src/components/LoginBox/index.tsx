a323af0781fe978610908f85ecd14cb65ad179b9
Arruda2904 <62766364+Arruda2904@users.noreply.github.com>
2021-04-18 18:40:34 -0300

    some minor css changes

--- src/components/LoginBox/index.tsx
+++ src/components/LoginBox/index.tsx
@@ -1,17 +1,16 @@
 import React from 'react';
-import { Container, BoxForm, Component, CreateAccount } from './styles'
+import { Container, Component, CreateAccount } from './styles'
 import { Link } from "react-router-dom";
 
 export default function LoginBox() {
     return (
         <Container>
             <Component>
+                <h1>Portal Ferramong</h1>
                 <form className="contact-form" /*onSubmit={sendLogin} action="efetuar-login.php" method="post"*/ >
-                    <BoxForm>
                         <input type="text" name="cpf" placeholder="CPF"/>
                         <input type="password" name="password" placeholder="Senha" />
                         <Link to="/"><input type="submit" value="Entrar" id="button"/></Link>
-                    </BoxForm>
                 </form>
             <CreateAccount>
                 <h3>Não possui uma conta ainda? Cadastre-se <Link to="/">aqui</Link> </h3>
--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import LoginBox from '../../components/LoginBox'
-import Menu from '../../components/Menu'
 
 const Login = () => {
     return (
         <div>
-            <Menu />
             <LoginBox />
         </div>
     );

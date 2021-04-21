509fe2cb46ba9c4f8bf13c496c1eedeafe8cb002
Arruda2904 <62766364+Arruda2904@users.noreply.github.com>
2021-04-21 17:07:07 -0300

    logo adicionada no menu

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,22 +1,24 @@
 import React from 'react';
-import { Container, NavBar, Item, SearchBar,Login } from './styles'
+import { Container, NavBar, Item, SearchBar, Login } from './styles'
 import SearchIcon from '@material-ui/icons/Search';
 import { Link } from "react-router-dom";
 //import VisitSchedulerModal from '../VisitScheduler';
+import Logo from "../../data/ferramong_logo.png"
 
 export default function Menu() {
     return (
         <Container>
             <NavBar>
-            <Link className="link" to="/addtool"><Item>Adicionar ferramenta</Item></Link>
-            <Link className="link" to="/listtool"><Item>Listar ferramentas</Item></Link>
-            <Link className="link" to="/listpurchase"><Item>Listar compras</Item></Link>
-            {/* <VisitSchedulerModal userId = "123456789"></VisitSchedulerModal> */}
+                <Link to='/'><img src={Logo}></img></Link>
+                <Link className="link" to="/addtool"><Item>Adicionar ferramenta</Item></Link>
+                <Link className="link" to="/listtool"><Item>Listar ferramentas</Item></Link>
+                <Link className="link" to="/listpurchase"><Item>Listar compras</Item></Link>
+                {/* <VisitSchedulerModal userId = "123456789"></VisitSchedulerModal> */}
                 {/* <SearchBar>
                     <input type="text" placeholder="Cortador de grama..." />
                     <button type="submit"><SearchIcon/></button>
                 </SearchBar> */}
-            <Link className="link" to="/listrent"><Item>Listar empréstimos</Item></Link>
+                <Link className="link" to="/listrent"><Item>Listar empréstimos</Item></Link>
                 <Link className="link" to="/"><Login>Entrar</Login></Link>
             </NavBar>
         </Container>

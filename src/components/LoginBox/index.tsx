e488a611a618d81a4198cc1adfda5da249faa762
Arruda2904 <62766364+Arruda2904@users.noreply.github.com>
2021-04-22 16:58:22 -0300

    Login e singUp funcionando com o front

--- src/components/LoginBox/index.tsx
+++ src/components/LoginBox/index.tsx
@@ -1,14 +1,32 @@
 import React from 'react';
 import { Container, Component, CreateAccount } from './styles'
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import { useForm } from "react-hook-form";
+import axios from 'axios'
 
 export default function LoginBox() {
 
     const {register, handleSubmit, formState: { errors }} = useForm();
 
+    const history = useHistory();
+
     const onSubmit = (data) => {
-        console.log(data)
+        console.log('DADOS ENVIADOS PRA API:');
+        console.log(data);
+
+        axios.post('https://ferramong-auth.herokuapp.com/authenticator/login',{
+            cpf: data.cpf,
+            password: data.password,
+        })
+        .then(response => {
+            console.log('DADOS DE RESPOSTA:');
+            console.log(response);
+            history.push('./addtool')
+        })
+        .catch(error => {
+            console.log('DADOS DE ERRO:');
+            console.log(error);
+        })
     }
 
     return (
@@ -19,8 +37,7 @@ export default function LoginBox() {
                         {errors.cpf && errors.cpf.type === "required" && <span>Indique seu cpf</span>}
                         <input type="password" placeholder="Senha" {...register("password", { required: true })}/>
                         {errors.password && errors.password.type === "required" && <span>Indique sua senha</span>}
-                        {/*<Link to={"/"}>*/}<input type="submit" value="Entrar" id="button"/> {/*</Link> com esse link pra outra página nao funcionava no console,tem que ver se na api vai*/}
-                </form>
+                        <input type="submit" value="Entrar" id="button"/>                 </form>
             <CreateAccount>
                 <h3>Não possui uma conta ainda? Cadastre-se <Link to="/register">aqui</Link> </h3>
                 <h3>Esqueceu sua senha? Clique <Link to="/forgotpassword">aqui</Link></h3>

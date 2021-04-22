b0716b5635b3f4cfdcea0aa2d2c2d769cd59bebc
Arruda2904 <62766364+Arruda2904@users.noreply.github.com>
2021-04-22 14:59:20 -0300

    quando esquecer a senha, pode mudar para página forgotpassword e aqui vai mandar cpf,secret answer e nova senha

--- src/components/ForgotPasswordBox/index.tsx
+++ src/components/ForgotPasswordBox/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Container, Component,} from './styles'
+import { Container, Component, } from './styles'
 import { Link } from "react-router-dom";
 import { useForm } from "react-hook-form";
 
 export default function LoginBox() {
 
-    const {register, handleSubmit} = useForm();
+    const { register, handleSubmit } = useForm();
 
     const onSubmit = (data) => {
         console.log(data)
@@ -15,9 +15,11 @@ export default function LoginBox() {
         <Container>
             <Component>
                 <form className="contact-form" onSubmit={handleSubmit(onSubmit)}>
-                        <h3>Sua pergunta secreta</h3>
-                        <input type="text" placeholder="Resposta secreta" {...register("secretAnswer")}/>
-                        {/*<Link to={"/"}>*/}<input type="submit" value="Responder" id="button"/> {/*</Link> com esse link pra outra página nao funcionava no console,tem que ver se na api vai*/}
+                    <input type="text" placeholder="Digite seu CPF" {...register("cpf")} />
+                    <h3>Sua pergunta secreta</h3>
+                    <input type="text" placeholder="Resposta secreta" {...register("secretAnswer")} />
+                    <input type="password" placeholder="Nova senha" {...register("newPassword")} />
+                    {/*<Link to={"/"}>*/}<input type="submit" value="Mudar senha" id="button" /> {/*</Link> com esse link pra outra página nao funcionava no console,tem que ver se na api vai*/}
                 </form>
             </Component>
         </Container>

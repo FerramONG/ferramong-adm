a7f7b4148aee5a2318dd5f6eff08bf909ea3d3fc
Arruda2904 <62766364+Arruda2904@users.noreply.github.com>
2021-04-26 11:31:24 -0300

    Página de pesquisar ferramentas 100% implementada

--- src/components/ToolBox/index.tsx
+++ src/components/ToolBox/index.tsx
@@ -1,17 +1,28 @@
 import React, { useState } from 'react';
-import { Container, MainInfo, LeftPannel, NameCategory, ExtraInfo, Send } from './styles'
+import { Container, MainInfo, LeftPannel, NameCategory, ExtraInfo} from './styles'
 import AddIcon from '@material-ui/icons/Add';
 import RemoveIcon from '@material-ui/icons/Remove';
+import AddToolSchedulerStart from '../AddToolSchedulerStart';
+import AddToolSchedulerEnd from '../AddToolSchedulerEnd';
+import { useLogin } from '../../context/GlobalState'
+import { useHistory } from 'react-router-dom'
+import axios from 'axios'
 
 interface ToolProps {
     name: string;
     category: string;
+    ownerId: number;
     price: string;
     utility: string;
     use: string;
+    toolId: number;
+    availableUntil: string;
 }
 
 export default function ToolBox(props: ToolProps) {
+    const { userId, setUserId, token, setToken, startDateTool, setStartDateTool, endDateTool, setEndDateTool } = useLogin();
+    let ownerId = props.ownerId;
+    let rentedToolId = props.toolId
 
     const [expandedContainer, setExpandedContainer] = useState(false);
 
@@ -39,11 +50,10 @@ export default function ToolBox(props: ToolProps) {
             <ExtraInfo className={expandedContainer ? "display" : "noDisplay"}>
                 <h4><b>Utilidade: </b>{props.utility}</h4>
                 <h4><b>Como usar: </b>{props.use}</h4>
+                <h4><b>Disponível até: </b>{props.availableUntil}</h4>
+                <h4><b>ID da ferramenta: </b>{props.toolId}</h4>
+                <h4><b>ID do dono: </b>{props.ownerId}</h4>
             </ExtraInfo>
-
-            <Send className={expandedContainer ? "display" : "noDisplay"}>
-                <button>Quero!</button>
-            </Send>
 
             <button type="button" className="expandedContainerButton" onClick={() => changeButton()}>
                 {expandedContainer ? <RemoveIcon /> : <AddIcon />}
--- src/pages/ListTool/index.tsx
+++ src/pages/ListTool/index.tsx
@@ -1,39 +1,65 @@
 import React, { useEffect, useState } from 'react';
-import Menu from '../../components/Menu'
+import Menu from '../../components/Menu';
 import ToolBox from '../../components/ToolBox';
 import data from '../../data/ToolsInfo'
 import { ToolsContainer } from './styles'
+import { useLogin } from '../../context/GlobalState'
+import { useHistory } from 'react-router-dom'
+import { useForm } from "react-hook-form";
+import axios from 'axios'
 
-const ListTool = () => {
+
+const Home = () => {
+
+    const { userId, setUserId, token, setToken } = useLogin();
+
+    console.log("Está logado no HOME: " + userId + ' Com o token: ' + token);
+    let history = useHistory();
+
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const [searchedTool, setSearchedTool] = useState([]);
+
+    const onSearch = (data) => {
+        axios.get('https://ferramong-tools-manager.herokuapp.com/search/by-tool?toolName='+data.toolName+'&fetchAvailable=true&fetchRented=true')
+        .then(response => {
+            console.log('------FERRAMENTAS DISPONÍVEIS:-----');
+            console.log(response.data);
+            setSearchedTool(response.data.availableTools)
+        })
+        .catch(error => {
+            console.log('DADOS DE ERRO FERRAMENTAS PESQUISADAS:');
+            console.log(error);
+        })
+    }
 
     const [search, setSearch] = useState('');
-    //const [filteredTools, setFilteredTools] = useState([]);
-
-    // useEffect( ()=>{
-    //     setFilteredTools(
-    //         data.filter( tool => {
-    //             return tool.name.toLowerCase().includes(search.toLowerCase())
-    //         })
-    //     )
-    // },[search,filteredTools])
 
     const filteredTools = data.filter(tool => {
         return tool.name.toLowerCase().includes(search.toLowerCase())
     })
 
     return (
-        <div>
-            <Menu />  
+        <React.Fragment>
+            <Menu />
             <ToolsContainer>
-            <input type="text" placeholder="Pesquisar ferramenta" onChange={e => setSearch(e.target.value)} />
-                {filteredTools.map(purchase => {
-                    return (
-                        <ToolBox name={purchase.name} category={purchase.category}
-                    price={purchase.price} utility={purchase.utility} use={purchase.use}></ToolBox>
+                <form className="search-form" onSubmit={handleSubmit(onSearch)}>
+                    <div>
+                        <input type="text" placeholder="Pesquisar ferramenta" {...register("toolName", { required: true })} />
+                        <input type="submit" value="Buscar" id="button" />
+                    </div>
+                    {errors.toolName && errors.toolName.type === "required" && <span>Digite uma ferramenta</span>}
+                </form>
+
+                {searchedTool.map(tool => {
+                    return(
+                        <ToolBox name={tool['name']} category={tool['category']} ownerId={tool['ownerId']}
+                        price={tool['price']} utility={tool['description']} use={tool['instructions']} 
+                        toolId={tool['id']} availableUntil={tool['availableUntil']}/>
                     )
-                })}
+                })
+                }
             </ToolsContainer>
-        </div>
+        </React.Fragment>
     );
 }
-export default ListTool;
+export default Home;
